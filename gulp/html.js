c790cb21ef54f0c64676c0cdeeb8e6ed7a0046d2
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-07-29 12:16:00 -0400

    - improvements to the use of the generated html import, now auto importing the components css vs loading it as a normal style sheet (allowing :host and other polymer specific tags to be accessible natively), and ensuring the css/js is fully scoped for the current component while still keeping the source files more flexible

--- gulp/html.js
+++ gulp/html.js
@@ -15,17 +15,22 @@ let htmlCompile = function (taskName, dist = false) {
 // Build HTML for distribution.
   gulp.task(taskName, ()=> {
     gulp.src(global.paths.src + compHtmlFilename)
-      .pipe(gulpif(dist, replace('/bower_components', '../..')))
-      .pipe(rename({
-        suffix: '-module'
-      }))
+      .pipe(replace('</dom-module>', '' +
+        '  <link rel="import" type="css" href="css/' + global.comp.name + ".css\">\n" +
+        '  <script src="js/' + global.comp.name + ".js\"></script>\n" +
+        '</dom-module>'))
+      //.pipe(rename({
+      //  suffix: '-module'
+      //}))
+      .pipe(gulpif(dist, replace(global.comp.name + '.js', global.comp.name + '.min.js')))
+      .pipe(gulpif(dist, replace(global.comp.name + '.css', global.comp.name + '.min.css')))
       .pipe(gulp.dest(dest));
 
-    gulp.src([global.paths.src + 'index.html', global.paths.buildSrcDir + compHtmlFilename])
+    gulp.src([global.paths.src + 'index.html'])
       .pipe(gulpif(dist, replace('webcomponents-lite.js', 'webcomponents-lite.min.js')))
       .pipe(gulpif(dist, replace('/bower_components', '../..')))
-      .pipe(gulpif(dist, replace(global.comp.name+'.js', global.comp.name+'.min.js')))
-      .pipe(gulpif(dist, replace(global.comp.name+'.css', global.comp.name+'.min.css')))
+      .pipe(gulpif(dist, replace(global.comp.name + '.js', global.comp.name + '.min.js')))
+      .pipe(gulpif(dist, replace(global.comp.name + '.css', global.comp.name + '.min.css')))
       .pipe(gulpif(dist, minifyHtml()))
       .pipe(gulp.dest(dest));
   });
