d5a21c1ddf896625647a0d08144cbee6b4a2413a
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-08-17 16:44:18 -0400

    - corrections to abortion

--- src/hence-user.js
+++ src/hence-user.js
@@ -5,42 +5,34 @@
 import console from 'consoler';
 import {HenceSchema} from 'hence-polycore';
 
-let is = 'hence-user';
-
 /**
  * HenceUser Component
  * @constructor
  */
 let HenceUser = HenceSchema({
-  is, // auto set as is : is, es6 laziness joy!
+  is: 'hence-user',
   /********************************************************************************************************************
    * Initialization
    ********************************************************************************************************************/
   properties: {},
-
-  /*********************************************************************************************************************
-   * Event Listeners
-   ********************************************************************************************************************/
-
-  /**
-   * When working with listeners, if their target element doesn’t exist on the DOM you get a very basic nonspecific
-   * error 'Uncaught TypeError: Invalid value used as weak map key’!  Make sure to review the listeners you set up
-   * against you DOM elements. By default listeners look for IDs on elements so ‘myButton.tap’ will watch click/touches
-   * on a #myButton element in the component
-   */
-  listeners: {},
 
 
   /*********************************************************************************************************************
    * Element Behaviour
    ********************************************************************************************************************/
 
-    _executeQuery() {
+  /**
+   * Determine the action and query passed in to handle the request and provide whatever state data is required along.
+   * Executed by the ```render()``` function
+   *
+   * @private
+   */
+    _executeQuery(done) {
     let self = this;
-    let query = self.query || {};
-    let results = self.results || [];
+    let {action,query} = self;
+    let results = [];
 
-    switch (this.action) {
+    switch (action) {
       case 'getUser':
         // do query
 
@@ -68,11 +60,8 @@ let HenceUser = HenceSchema({
         break;
     }
 
-    //console.log('_executeQuery ', results, query);
-
-    return results;
+    done(null, results);
   }
 });
 
-export {is};
 export default HenceUser;
